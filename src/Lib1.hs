d11e7033520c17960c5b3f4252935744c75c2fb3
domanteli0 <domantelio@proton.me>
2022-09-23 13:51:39 +0300

    `show` for `State` now shows more readable output + some clean ups

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -4,33 +4,22 @@ module Lib1(
 ) where
 
 import Types
-import           Data.Char
-import           Data.List
-import           Data.Maybe
--- import Lib1 (gameStart)
--- import Control.Lens.Internal.Deque (fromList)
--- import Lib1 (gameStart)
+import Data.Char
+import Data.List (delete)
 -- This is a state of your game.
 -- It must contain all values you might need during a game:
 -- number of occupied rows/cols, hints, occupied cells,..
 -- You can change the right hand side as you wish but please
 -- keep the type name as is
--- data State = State [String] -- OG
 type Hinted = Coord
 type Toggled = Coord
 type ColNo = [Int]
 type RowNo = [Int]
 type HintNo = Int
 data State = State [Toggled] [Hinted] ColNo RowNo HintNo
-    deriving Show
 
--- C like struct'as
-{-
-struct State {
-    Check toggled[], // <- tai kas toggle'inta
-    Check hinted[]  // <- tai kas gauta iš hint
-}
--}
+instance Show State where
+    show (State ts hs c r h) = "Toggled: " ++ show ts ++ "\nHinted: " ++ show hs ++ "\nCol: " ++ show c ++ "\nRow: " ++ show r ++ "\nNo of hints: " ++ show h
 
 -- IMPLEMENT
 -- This is very initial state of your program
@@ -44,23 +33,32 @@ gameStart :: State -> Document -> State
 gameStart _ doc = State [] [] colNo rowNo hintNo
     where
         hintNo = fromDInteger (findDMap "number_of_hints" dMap)
-        rowNo = map (fromDInteger) $ fromDList (findDMap "occupied_rows" dMap)
-        colNo = map (fromDInteger) $ fromDList (findDMap "occupied_cols" dMap)
+        rowNo = map fromDInteger $ fromDList (findDMap "occupied_rows" dMap)
+        colNo = map fromDInteger $ fromDList (findDMap "occupied_cols" dMap)
         dMap = fromDMap doc
 
 findDMap :: String -> [(String, Document)] -> Document
-findDMap key [] = error "Unable to find"
 findDMap key ((dKey, doc):xs) = if dKey == key then doc else findDMap key xs
+findDMap _ _ = error "Unable to find value with specified key"
+
 fromDMap :: Document -> [(String, Document)]
 fromDMap (DMap m) = m
+fromDMap _ = error "This Document is not a DMap"
 fromDList :: Document -> [Document]
 fromDList (DList l) = l
+fromDList _ = error "This Document is not a DList"
 fromDInteger :: Document -> Int
 fromDInteger (DInteger i) = i
+fromDInteger _ = error "This Document is not a DInteger"
 fromDString :: Document -> String
 fromDString (DString i) = i
-fromDNull :: Document -> IO a
-fromDNull (DNull) = error "Null"
+fromDString _ = error "This Document is not a DString"
+fromDNull :: Document -> Maybe a
+fromDNull DNull = Nothing
+fromDNull _ = error "This Document is not a DNull"
+isDNull :: Document -> Bool
+isDNull DNull = True
+isDNull _ = False
 
 -- IMPLEMENT
 -- renders your game board
@@ -88,21 +86,21 @@ toggle (State ts hs c r h) stringS = State ts' hs c r h
                 row' = decimalStringToInt (last stringS)
         ts' = if newToggle `elem` ts then delete newToggle ts else newToggle : ts
 
---Parsing String to Int         
+--Parsing String to Int
 stringToInt:: Int -> String -> Int
 stringToInt base digits
-    = sign * (foldl acc 0 $ concatMap digToInt digits1)
+    = sign * foldl acc 0 (concatMap digToInt digits1)
       where
-      splitSign ('-' : ds) = ((-1), ds)
+      splitSign ('-' : ds) = (-1, ds)
       splitSign ('+' : ds) = ( 1  , ds)
       splitSign ds         = ( 1  , ds)
       (sign, digits1)      = splitSign digits
       digToInt c
-          | c >= '0' && c <= '9'
+          | isDigit c
               = [ord c - ord '0']
-          | c >= 'A' && c <= 'Z'
+          | isAsciiUpper c
               =  [ord c - ord 'A' + 10]
-          | c >= 'a' && c <= 'z'
+          | isAsciiLower c
               =  [ord c - ord 'a' + 10]
           | otherwise = []
       acc i1 i0= i1 * base + i0

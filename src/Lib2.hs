4739b32a71f0b2c27a3d30dec0e73347ae91471d
domanteli0 <domantelio@proton.me>
2022-10-06 22:28:03 +0300

    Implemented needed scalar types in `renderDocument`

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -2,7 +2,7 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Lib2(renderDocument, hint, gameStart) where
 
-import Types ( ToDocument(..), Document, Check )
+import Types ( ToDocument(..), Document(..), Check )
 import Lib1 (State(..))
 
 -- IMPLEMENT
@@ -11,18 +11,25 @@ instance ToDocument Check where
     toDocument a = DNull
 
 -- IMPLEMENT
+-- ASSIGNEE: DOMANTAS
 -- Renders document to yaml
 renderDocument :: Document -> String
-renderDocument _ = error "Implement me"
+renderDocument doc = "---\n" ++ renderDoc doc 0
+    where
+        renderDoc DNull level = "null"
+        renderDoc (DString s) level = '\'' : s ++ ['\'']
+        renderDoc (DInteger i) level = show i
+        renderDoc (DList l) level = error "Implement me"
+        renderDoc (DMap m) level = error "Implement me"
 
 -- IMPLEMENT
 -- This adds game data to initial state
 -- Errors are reported via Either but not error
 gameStart :: State -> Document -> Either String State
-gameStart (State l) d = Right $ State $ ("Game started: " ++ show d) : l
+gameStart (State ts hs c r h) d = Left "TODO: IMPLEMENT GAMESTART"
 
 -- IMPLEMENT
 -- Adds hint data to the game state
 -- Errors are reported via Either but not error
 hint :: State -> Document -> Either String State
-hint (State l) h = Right $ State $ ("Hint " ++ show h) : l
+hint (State ts hs c r h) d = Left "TODO: IMPLEMENT HINT"

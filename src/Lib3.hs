fce0f78c22da0304a36afdd73707abf91e756caa
Domanteli0 <domantelio@proton.me>
2022-11-04 15:47:58 +0200

    Added dummy implementations
    so that the project would at least compile.

--- src/Lib3.hs
+++ src/Lib3.hs
@@ -2,32 +2,38 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Lib3(hint, gameStart, parseDocument, GameStart, Hint) where
 
-import Types ( Document, FromDocument, fromDocument )
+import Types ( Document(..), FromDocument, fromDocument )
 import Lib1 (State(..))
 
 -- IMPLEMENT
 -- Parses a document from yaml
 parseDocument :: String -> Either String Document
-parseDocument _ = Left "Implement me"
+parseDocument str = error "NOT IMPLEMENTED"
 
 -- IMPLEMENT
 -- Change right hand side as you wish
 -- You will have to create an instance of FromDocument
 data GameStart = GameStart deriving Show
+instance FromDocument GameStart where
+    fromDocument _ = error "NOT IMPLEMENTED"
 
 -- This adds game data to initial state
 -- Errors are not reported since GameStart is already totally valid adt
 -- containing all fields needed
 gameStart :: State -> GameStart -> State
-gameStart (State l) d = State $ ("Game started: " ++ show d) : l
+gameStart s _ = s
+-- gameStart (State l) d = State $ ("Game started: " ++ show d) : l
 
 -- IMPLEMENT
 -- Change right hand side as you wish
 -- You will have to create an instance of FromDocument
 data Hint = Hint deriving Show
+instance FromDocument Hint where
+    fromDocument _ = error "NOT IMPLEMENTED"
 
 -- Adds hint data to the game state
 -- Errors are not reported since GameStart is already totally valid adt
 -- containing all fields needed
 hint :: State -> Hint -> State
-hint (State l) h = State $ ("Hint " ++ show h) : l
+hint s _ = s
+-- hint (State l) h = State $ ("Hint " ++ show h) : l

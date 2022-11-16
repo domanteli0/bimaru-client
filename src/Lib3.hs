34fb7720ceb75447c439ed4d4696dc91676c99da
Aleksandra Kondratjeva <89353862+artnaxel@users.noreply.github.com>
2022-11-16 22:23:31 +0200

    Implemented `gameStart`, it in need of refactoring
    Will try to refactor `Left` and `Right`

--- src/Lib3.hs
+++ src/Lib3.hs
@@ -10,6 +10,10 @@ import Types ( Document(..), FromDocument, fromDocument )
 import Lib1 (State(..))
 import Data.List ( isPrefixOf )
 
+type ColNo = [Int]
+type RowNo = [Int]
+type HintNo = Int
+
 -- YamlToken is purposefully not recursive
 data Token =
       TokenNull
@@ -94,16 +98,79 @@ parseDocument str = Left "NOT IMPLEMENTED"
 -- IMPLEMENT
 -- Change right hand side as you wish
 -- You will have to create an instance of FromDocument
-data GameStart = GameStart deriving Show
+data GameStart = GameStart ColNo RowNo HintNo
 instance FromDocument GameStart where
-    fromDocument _ = error "NOT IMPLEMENTED"
+    fromDocument  doc = do
+        colNo <- case fromDMap doc of
+            Left msg1 -> Left msg1
+            Right q1 -> case findDMap "occupied_cols" q1 of
+                Left msg2 -> Left msg2
+                Right q2 -> case fromDList q2 of
+                    Left msg3 -> Left msg3
+                    Right q3 -> case mapM fromDInteger q3 of
+                        Left msg4 -> Left msg4
+                        Right q4 -> case checkLength q4 of
+                            Left msg5 -> Left msg5
+                            Right q5 -> Right q5
+
+        rowNo <- case fromDMap doc of
+            Left msg1 -> Left msg1
+            Right q1 -> case findDMap "occupied_rows" q1 of
+                Left msg2 -> Left msg2
+                Right q2 -> case fromDList q2 of
+                    Left msg3 -> Left msg3
+                    Right q3 -> case mapM fromDInteger q3 of
+                        Left msg4 -> Left msg4
+                        Right q4 -> case checkLength q4 of
+                            Left msg5 -> Left msg5
+                            Right q5 -> Right q5
+
+        hintNo <- case fromDMap doc of
+            Left msg1 -> Left msg1
+            Right q1 -> case findDMap "number_of_hints" q1 of
+                Left msg2 -> Left msg2
+                Right q2 -> case fromDInteger q2 of
+                    Left msg3 -> Left msg3
+                    Right q3 -> Right q3
+
+        return $ GameStart colNo rowNo hintNo
+
+checkLength :: [Int] -> Either String [Int]
+checkLength list = if length list == 10 then Right list else Left "Number of rows or cols != 10"
+
+findDMap :: String -> [(String, Document)] -> Either String Document
+findDMap key ((dKey, doc):xs) =   if dKey ==  key then Right doc else findDMap key xs
+findDMap _ _ = Left "Unable to find value with specified key"
+
+fromDMap :: Document -> Either String [(String, Document)]
+fromDMap (DMap m) = Right m
+fromDMap _ = Left "Document is not a DMap"
+
+fromDList :: Document -> Either String [Document]
+fromDList (DList l) = Right l
+fromDList _ = Left "Document is not a DList"
+
+fromDInteger :: Document -> Either String Int
+fromDInteger (DInteger i) = Right i
+fromDInteger _ = Left "Document is not a DInteger"
+
+fromDString :: Document -> Either String String
+fromDString (DString i) = Right i
+fromDString _ = Left "Document is not a DString"
+
+fromDNull :: Document -> Either String (Maybe a)
+fromDNull DNull = Right Nothing
+fromDNull _ = Left "Document is not a DNull"
+
+isDNull :: Document -> Bool
+isDNull DNull = True
+isDNull _ = False
 
 -- This adds game data to initial state
 -- Errors are not reported since GameStart is already totally valid adt
 -- containing all fields needed
 gameStart :: State -> GameStart -> State
-gameStart s _ = s
--- gameStart (State l) d = State $ ("Game started: " ++ show d) : l
+gameStart _ (GameStart colNo rowNo hintNo) = State [] [] colNo rowNo hintNo
 
 -- IMPLEMENT
 -- Change right hand side as you wish
